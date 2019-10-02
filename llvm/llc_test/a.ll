2a20844054a8437e86d67a067ecef15435092b04
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2019-10-02 20:58:01 +0300

    Done ref type and array reference passing

--- llvm/llc_test/a.ll
+++ llvm/llc_test/a.ll
@@ -7,16 +7,14 @@ declare void @writeString(i8*)
 
 define void @main() {
 entry:
-  %y = alloca i32
-  %a = alloca [10 x i32]
-  store i32 0, i32* %y
-  %lvalue_ptr = getelementptr [10 x i32], [10 x i32]* %a, i32 0, i32 0
-  store i32 666, i32* %lvalue_ptr
-  %lvalue_ptr1 = getelementptr [10 x i32], [10 x i32]* %a, i32 0, i32 1
-  store i32 4, i32* %lvalue_ptr1
-  %rvalue = load [10 x i32], [10 x i32]* %a
-  call void @hello([10 x i32] %rvalue)
-  %lvalue_ptr2 = getelementptr [10 x i32], [10 x i32]* %a, i32 0, i32 1
+  %y = alloca [10 x i32]
+  %lvalue_ptr = getelementptr [10 x i32], [10 x i32]* %y, i32 0, i32 0
+  store i32 2, i32* %lvalue_ptr
+  %lvalue_ptr1 = getelementptr [10 x i32], [10 x i32]* %y, i32 0, i32 0
+  %rvalue = load i32, i32* %lvalue_ptr1
+  call void @writeInteger(i32 %rvalue)
+  call void @hello([10 x i32]* %y)
+  %lvalue_ptr2 = getelementptr [10 x i32], [10 x i32]* %y, i32 0, i32 0
   %rvalue3 = load i32, i32* %lvalue_ptr2
   call void @writeInteger(i32 %rvalue3)
   ret void
@@ -24,14 +22,9 @@ entry:
 
 declare void @writeInteger.1(i32)
 
-define void @hello([10 x i32] %x) {
+define void @hello([10 x i32]* %x) {
 entry:
-  %x1 = alloca [10 x i32]
-  store [10 x i32] %x, [10 x i32]* %x1
-  %lvalue_ptr = getelementptr [10 x i32], [10 x i32]* %x1, i32 0, i32 1
+  %lvalue_ptr = getelementptr [10 x i32], [10 x i32]* %x, i32 0, i32 0
   store i32 1, i32* %lvalue_ptr
-  %lvalue_ptr2 = getelementptr [10 x i32], [10 x i32]* %x1, i32 0, i32 0
-  %rvalue = load i32, i32* %lvalue_ptr2
-  call void @writeInteger(i32 %rvalue)
   ret void
 }
--- llvm/llc_test/a.s
+++ llvm/llc_test/a.s
@@ -6,47 +6,17 @@
 main:                                   # @main
 	.cfi_startproc
 # BB#0:                                 # %entry
-	pushq	%rbx
+	subq	$40, %rsp
 .Lcfi0:
-	.cfi_def_cfa_offset 16
-	subq	$48, %rsp
-.Lcfi1:
-	.cfi_def_cfa_offset 64
-.Lcfi2:
-	.cfi_offset %rbx, -16
-	movl	$0, 44(%rsp)
-	movabsq	$17179869850, %rax      # imm = 0x40000029A
-	movq	%rax, 4(%rsp)
-	movl	12(%rsp), %edx
-	movl	16(%rsp), %ecx
-	movl	20(%rsp), %r8d
-	movl	24(%rsp), %r9d
-	movl	28(%rsp), %r10d
-	movl	32(%rsp), %r11d
-	movl	36(%rsp), %eax
-	movl	40(%rsp), %ebx
-	movl	$666, %edi              # imm = 0x29A
-	movl	$4, %esi
-	pushq	%rbx
-.Lcfi3:
-	.cfi_adjust_cfa_offset 8
-	pushq	%rax
-.Lcfi4:
-	.cfi_adjust_cfa_offset 8
-	pushq	%r11
-.Lcfi5:
-	.cfi_adjust_cfa_offset 8
-	pushq	%r10
-.Lcfi6:
-	.cfi_adjust_cfa_offset 8
-	callq	hello
-	addq	$32, %rsp
-.Lcfi7:
-	.cfi_adjust_cfa_offset -32
-	movl	8(%rsp), %edi
+	.cfi_def_cfa_offset 48
+	movl	$2, (%rsp)
+	movl	$2, %edi
 	callq	writeInteger
-	addq	$48, %rsp
-	popq	%rbx
+	movq	%rsp, %rdi
+	callq	hello
+	movl	(%rsp), %edi
+	callq	writeInteger
+	addq	$40, %rsp
 	retq
 .Lfunc_end0:
 	.size	main, .Lfunc_end0-main
@@ -58,25 +28,7 @@ main:                                   # @main
 hello:                                  # @hello
 	.cfi_startproc
 # BB#0:                                 # %entry
-	subq	$40, %rsp
-.Lcfi8:
-	.cfi_def_cfa_offset 48
-	movl	48(%rsp), %r10d
-	movl	56(%rsp), %r11d
-	movl	64(%rsp), %eax
-	movl	72(%rsp), %esi
-	movl	%esi, 36(%rsp)
-	movl	%eax, 32(%rsp)
-	movl	%r11d, 28(%rsp)
-	movl	%r10d, 24(%rsp)
-	movl	%r9d, 20(%rsp)
-	movl	%r8d, 16(%rsp)
-	movl	%ecx, 12(%rsp)
-	movl	%edx, 8(%rsp)
-	movl	%edi, (%rsp)
-	movl	$1, 4(%rsp)
-	callq	writeInteger
-	addq	$40, %rsp
+	movl	$1, (%rdi)
 	retq
 .Lfunc_end1:
 	.size	hello, .Lfunc_end1-hello
